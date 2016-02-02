3dbd4e350ddafe9f28d18df4a9df29c91a0aef5b
krupin.dv <krupin.dv19@gmail.com>
2016-02-02 17:29:34 +0300

    GroupsForm, layout.html, subscribers_intersection.html

---
+++ intersections/forms.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from django import forms
+
+
+
+class GroupsForm(forms.Form):
+    links = forms.CharField(label=u'Ссылки', required=True,
+                            widget=forms.Textarea(attrs={
+                                'class': 'form-control',
+                                'rows': '5',
+                                'style': 'width: 100% !important;'
+                            }))
--- intersections/views.py
+++ intersections/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import View
 from django.views.generic.base import TemplateResponseMixin
 
+from . forms import GroupsForm
 
 
 class SubscribersIntersection(View, TemplateResponseMixin):
@@ -8,4 +9,4 @@ class SubscribersIntersection(View, TemplateResponseMixin):
     template_name = 'intersections/subscribers_intersection.html'
 
     def get(self, request):
-        return self.render_to_response({})
+        return self.render_to_response({'form': GroupsForm})
