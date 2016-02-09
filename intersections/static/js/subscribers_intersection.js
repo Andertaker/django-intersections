a70e26088f19d7223e61c2469092c4cbfec50f3e
krupin.dv <krupin.dv19@gmail.com>
2016-02-09 20:44:24 +0300

    synchonization fix

--- intersections/static/js/subscribers_intersection.js
+++ intersections/static/js/subscribers_intersection.js
@@ -1,46 +1,80 @@
+$.ajaxPrefilter(function( options ) {
+    //console.log(options.async);
+    options.async = false;
+    //console.log(options.async);
+});
+
+
+
+
 $("#group_form").submit(function(e){
     e.preventDefault();
 
     links = $(this).find('#id_links').val();
-    links_arr = links.split(/\s+/);
+    links_arr = links.trim().split(/\s+/);
 
     $subscribers_table = $('#subscribers_table tbody');
+    $subscribers_table.html('');
+    
+    var group;
+    var social;
+    
+    i = 0;
+    
 
-    for (i in links_arr) {
-        link = links_arr[i]
-        console.log(link);
+    //for (i in links_arr) {
 
-        $.post(FETCH_GROUP_URL, {'link': link}, function(response) {
-            console.log(response);
-            group = response['group']
+    iterate(i);
+            
+        
+        //response = result.responseJSON
+        //console.log(result);
 
-            cells_arr = [
-                '<a href="%s">%s</a>'.replace(/%s/g, link),
-                group['name'],
-                group['members_in_db_count'],
-                group['members_count'],
-            ]
-
-            cells_arr = $.map(cells_arr, function(el) {
-               return $('<td>').html(el);
-            });
-            $row = $('<tr>').html(cells_arr);
-            $subscribers_table.append($row)
-
-            if (!group['members_fetched_date']) {
-                fetch_members(response['social'], group['id']);
-            }
-
-        });
-
-    }
 });
+
+
+function iterate(i) {
+    link = links_arr[i]
+    console.log(link);
+
+    $.post(FETCH_GROUP_URL, {'link': link}, function(response) {
+        console.log(response);
+        social = response['social'];
+        group = response['group'];
+
+        cells_arr = [
+            '<a href="%s">%s</a>'.replace(/%s/g, link),
+            group['name'],
+            group['members_in_db_count'],
+            group['members_count'],
+        ]
+
+        cells_arr = $.map(cells_arr, function(el) {
+           return $('<td>').html(el);
+        });
+        $row = $('<tr>').html(cells_arr);
+        $subscribers_table.append($row);
+        
+        if (!group['members_fetched_date']) {
+            fetch_members(social, group['id']);
+        }
+        else {
+            i++;
+            console.log(i);
+            if (i < links_arr.length)
+                iterate(i); // iterate next link
+        }
+    });
+        
+}
 
 
 
 function fetch_members(social, group_id) {
     url = FETCH_GROUP_MEMBERS_MONITOR_URL + social + '/' + group_id + '/'
     $cell = $subscribers_table.find('tr:last td:nth-child(3)');
+
+    var group;
 
     settings = {
         'url': url,
@@ -53,8 +87,13 @@ function fetch_members(social, group_id) {
 
     update_members = function(){
         $.get(settings);
-        if (group['members_fetched_date'])
+        if (group['members_fetched_date']) {
             clearInterval(intervalID)
+            i++;
+            console.log(i);
+            if (i < links_arr.length)
+                iterate(i); // iterate next link
+        }    
     }
     intervalID = setInterval(update_members, 5000)
     update_members(); // first start without delay
