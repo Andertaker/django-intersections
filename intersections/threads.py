ea23923ad6745ba5c8d57426d7275951e9d2a76f
krupin.dv <krupin.dv19@gmail.com>
2016-02-18 18:29:17 +0300

    VkFetchGroupMembersThread changed

--- intersections/threads.py
+++ intersections/threads.py
@@ -2,6 +2,10 @@ import threading
 
 from django.utils import timezone
 
+from vkontakte_api.api import api_call
+from vkontakte_users.models import User
+from vkontakte_users.signals import users_to_fetch, fetch_users
+
 
 def get_proccess_by_name(proccess_name):
 
@@ -17,6 +21,7 @@ class VkFetchGroupMembersThread(threading.Thread):
 
     group = None
     members_in_db_count = None
+    _user_ids = []
 
     def __init__(self, group, *args, **kwargs):
         threading.Thread.__init__(self, *args, **kwargs)
@@ -24,23 +29,33 @@ class VkFetchGroupMembersThread(threading.Thread):
         self.daemon = True
 
         self.group = group
-        self.members_in_db_count = group.members.count()
+        self.members_in_db_count = 0 # group.members.count()
+        self._user_ids = []
+
+    def fetch_members(self, group, offset=0, count=1000):
+        response = api_call('groups.getMembers', group_id=group.pk,
+                            fields='first_name,last_name,sex,bdate,country,city',
+                            offset=offset, count=count, v=5.9)
+        users = User.remote.parse_response_users(response, items_field='items')
+        ids = [u.pk for u in users]
+        return ids
 
     def run(self):
         group = self.group
         offset = self.members_in_db_count
 
         while True:
-            users = group.fetch_members(offset=offset, count=1000)
-
-            if len(users) == 0:
+            ids = self.fetch_members(group, offset=offset, count=1000)
+            if len(ids) == 0:
                 break
 
-            self.members_in_db_count += len(users) # TO FIX: does't not show real value
+            self._user_ids += ids
+            self.members_in_db_count += len(ids)
             offset += 1000
 
-        group.members_fetched_date = timezone.now()
-        group.save()
+        # save users
+        users_to_fetch.disconnect(receiver=fetch_users) # prevent fetch users work
+        group.members = self._user_ids
 
         if group.members_count > group.members.count():
             raise Exception("Error occured while fetch members for group %s" % group.pk)
